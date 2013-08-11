a40733e4d9ac4a2221c234c9f2612ac82e419f8f
Mike Ferg <mikeyferg@gmail.com>
2013-08-11 12:47:30 -0700

    address bar test

--- personalPage02/js/app.js
+++ personalPage02/js/app.js
@@ -1,7 +1,14 @@
-function hideAddressBar(){
-  if(document.documentElement.scrollHeight<window.outerHeight/window.devicePixelRatio)
-    document.documentElement.style.height=(window.outerHeight/window.devicePixelRatio)+'px';
-  	setTimeout(window.scrollTo(1,1),0);
-	}
-window.addEventListener("load",function(){hideAddressBar();});
-window.addEventListener("orientationchange",function(){hideAddressBar();});
+$(document).ready(function() {
+
+  if (navigator.userAgent.match(/Android/i)) {
+    window.scrollTo(0,0); // reset in case prev not scrolled  
+    var nPageH = $(document).height();
+    var nViewH = window.outerHeight;
+    if (nViewH > nPageH) {
+      nViewH -= 250;
+      $('BODY').css('height',nViewH + 'px');
+    }
+    window.scrollTo(0,1);
+  }
+
+});
